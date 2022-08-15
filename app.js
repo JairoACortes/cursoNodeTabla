16020a032a785795eec16bd67df95d5e09ffb031
Jairo Cortes <jcortesro@unal.edu.co>
2022-08-15 17:27:36 -0500

    delete comment

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ console.log("base = "+argv.base+"; listado = "+argv.l+"; Límite = "+argv.h)
 // const [,,arg3 = 'base=5'] = process.argv
 // const [,base] = arg3.split("=");
 
-// const base = 8;
+
 
 createFile(argv.b,argv.l,argv.h).then(a => console.log(a,"creado")).catch(err => console.log(err));
